48df6b3ac4d2be14a41f7e57f8979c6f21093702
Leighton Lauton <leighton.lauton@q7technology.com.au>
2019-09-24 19:55:30 +1000

    FIX #0003
    Added difficulties however it needs to be implemented correctly. It needs to adjust the Computer AI's starting cash in response to the difficulty set by the player.

--- setup.py
+++ setup.py
@@ -17,8 +17,10 @@ PLAYER_CSV_FILE = 'players.csv'
 # Variables
 booBotPlaying = False
 intNumOfPlayers = 0
+intDifficulty = 'Easy'
+intDifficultyModifier = 1
 
-players = []
+arrPlayers = []
 
 # Classes
 class Player:
@@ -36,7 +38,17 @@ class Bot:
         self.cards = []
 
 # Functions
-# Gets the number of players playing the game
+def selectDifficulty(index):
+    difficulty = {
+            1:'Easy',
+            2:'Medium',
+            3:'Hard'
+        }
+
+    return difficulty.get(index, "[ERROR]: Please choose one of the following:\nEasy being '1'\nMedium being '2'\nHard being '3'.")
+
+# Gets the number of players playing the game, the difficulty wished to be played and asks for input to establish
+# whether the player would like to add bots to be added to the game.
 def getNumOfPlayers():
     intNumOfPlayers = int(input("Please enter how many players would like to join the game: "))
     booBotPlaying = False
@@ -67,13 +79,18 @@ def getNumOfPlayers():
         else:
             booBotPlaying = False
 
-    return intNumOfPlayers, booBotPlaying
+    __ = input("Please enter what difficulty you would like to play on (Easy = 1, Medium = 2, Hard = 3): ")
+    intDifficulty = selectDifficulty(__)
+
+    
+
+    return intNumOfPlayers, booBotPlaying, intDifficulty
 
 # Creates the players
-def initPlayers(intNumOfPlayers, booBotPlaying):
+def initPlayers(intNumOfPlayers, booBotPlaying, intDifficulty):
     for i in range(intNumOfPlayers):
         player = Player(i)
-        players.append({'Token': player.token, 'Cash': player.cash,
+        arrPlayers.append({'Token': player.token, 'Cash': player.cash,
                         'Properties': player.properties, 'Cards': player.cards})
 
     intNumOfBots = MAX_PLAYERS - intNumOfPlayers
@@ -81,15 +98,18 @@ def initPlayers(intNumOfPlayers, booBotPlaying):
     if (intNumOfBots > 0 and booBotPlaying == True):
         for i in range(intNumOfBots):
             bot = Bot(i)
-            players.append({'Token': bot.token, 'Cash': bot.cash,
+            arrPlayers.append({'Token': bot.token, 'Cash': bot.cash,
                         'Properties': bot.properties, 'Cards': bot.cards})
-
-
-    __ = pd.DataFrame(players)
+        
+    __ = pd.DataFrame(arrPlayers)
+    print(__)
 
     return __
 # Code
-intNumOfPlayers, booBotPlaying = getNumOfPlayers()
-players = initPlayers(intNumOfPlayers, booBotPlaying)
-players.to_csv(PLAYER_CSV_FILE)
+intNumOfPlayers, booBotPlaying, intDifficulty = getNumOfPlayers()
+arrPlayers = initPlayers(intNumOfPlayers, booBotPlaying, intDifficulty)
+arrPlayers.to_csv(PLAYER_CSV_FILE)
 print("[SYSTEM]: SUCCESSFULLY SAVED PLAYER CSV FILE")
+
+# DEBUGGING
+print(selectDifficulty(1))
